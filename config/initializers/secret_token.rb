63048527468bfb03addeb641f443cecd3ef124bf
ffmaer <tz348@nyu.edu>
2012-03-24 17:06:38 -0400

    first commit
    Signed-off-by: ffmaer <tz348@nyu.edu>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Caption::Application.config.secret_token = '6a68f0fdc2c4300f258811f106abba31b8bd3448d48a16694b01edd7220820f9c6d9dab56bacb1601ff421a5e9585dde6e8d361fbf489d534549fa199f1e0e36'
